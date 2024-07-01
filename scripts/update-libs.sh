013d96eca249b6c991d42e67f73d4097192286cf
Kirill Kouzoubov <kirill888@gmail.com>
2024-07-01 11:23:20 +1000

    Switch datacube install to a commit (was branch)

--- scripts/update-libs.sh
+++ scripts/update-libs.sh
@@ -4,7 +4,7 @@ DEPS=(
   "s3fs"
   "odc-geo>=0.4.7"
   "odc-stac>=0.3.10rc3"
-  "git+https://github.com/opendatacube/datacube-core@hs-load"
+  "git+https://github.com/opendatacube/datacube-core@1866e55f764e810f581a5f5ff929f65d872bc4f4"
 
   #"git+https://github.com/opendatacube/odc-stac.git@hs-load"
   #"git+https://github.com/opendatacube/odc-geo.git@hs-load"
